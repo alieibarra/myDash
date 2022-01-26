9e9fdc295e6aabc7ffaf8023f3e086c148f316a1
Alie Ibarra <alieibarra@gmail.com>
2022-01-26 13:25:01 -0800

    Render Google Calendar

--- src/App.js
+++ src/App.js
@@ -2,27 +2,25 @@
 import './App.css';
 //import { useState } from 'react';
 import Tasks from './components/tasks.js';
-
-
-
-
+import Calendar from './components/calendar.js'
 
 const App = () => {
 
   //HARDWIRED DATA--TEMP👇🏼
   const TASKS = [
-    {id: 1, title: "Test Task 1", dueDate: "02/22/2022", priority: 1},
-    {id: 2, title: "Test Task 2", dueDate: "02/23/2022", priority: 2},
-    {id: 3, title: "Test Task 3", dueDate: "02/24/2022", priority: 3}, 
-    {id: 4, title: "Test Task 4", dueDate: "02/25/2022", priority: 4},
+    {id: 1, title: "Test Task 1", dueDate: "02/22/2022", priority: 4, completed: false},
+    {id: 2, title: "Test Task 2", dueDate: "02/23/2022", priority: 3, completed: false},
+    {id: 3, title: "Test Task 3", dueDate: "02/24/2022", priority: 2, completed: false}, 
+    {id: 4, title: "Test Task 4", dueDate: "02/25/2022", priority: 1, completed: false},
   ]; 
   //HARDWIRED DATA--TEMP👆🏼
 
   return (
     <div className="App">
-    <header> Alie's MyDash </header>
+    <header> myDash </header>
+      <Calendar/>
       <Tasks tasks={TASKS}/>
-      <a href ='https://calendar.google.com/calendar/u/0/r/day?tab=mc'> Today's Scehdule</a>
+      
     </div>
   );
 }
---
+++ src/components/calendar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+//import PropTypes from 'prop-types';
+
+const Calendar = () => {
+  return ([
+  <iframe 
+    title="google calendar" 
+    src="https://calendar.google.com/calendar/embed?height=1000&wkst=1&bgcolor=%23ffffff&ctz=America%2FLos_Angeles&showTitle=0&showPrint=0&mode=AGENDA&showCalendars=1&src=YWxpZWliYXJyYUBnbWFpbC5jb20&src=djdhc3JsbDdhczB2cHBzdGJsMWRmNjY5YzhAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&src=NG5ycjhjZWQzZ2UwNW5qMTI3bm9ycm5scGtAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&src=Y19rZ3VxbDZ1MXVqcjB0Z3I4c2Q0b2I1Nmc4c0Bncm91cC5jYWxlbmRhci5nb29nbGUuY29t&color=%2333ae06&color=%23F6BF26&color=%23C0CA33&color=%23039BE5" 
+    width="500" 
+    height="1000" 
+    frameborder="0" 
+    scrolling="no"/>,
+  <button> Add New Event </button>]);
+};
+
+export default Calendar;
